96feea14a91bed685b9f1ec05366116dee6ccb2d
João Dias <joao.dias@inm.pt>
2024-10-17 19:00:02 +0100

    styling and icons

--- src/lib/cards.js
+++ src/lib/cards.js
@@ -2,7 +2,7 @@ const suits = ['SPADES', 'CLUBS', 'DIAMONDS', 'HEARTS'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 const generateDeck = () => {
-	const deck = [];
+	const deck = []
 
 	// Loop through each suit and value to create the cards
 	suits.forEach(suit => {
@@ -12,21 +12,21 @@ const generateDeck = () => {
 				cardSuit: suit,                 // Card suit, e.g., 'SPADES', 'HEARTS'
 				cardValue: value                // Card value, e.g., 'A', '10', 'K'
 			};
-			deck.push(card);
-		});
-	});
+			deck.push(card)
+		})
+	})
 
-	return deck;
+	return deck
 }
 
-const shuffleDeck = (deck) => {
-	for (let i = deck.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[deck[i], deck[j]] = [deck[j], deck[i]];
-	}
-	console.log('here');
-	return deck;
-};
+// const shuffleDeck = (deck) => {
+// 	for (let i = deck.length - 1; i > 0; i--) {
+// 		const j = Math.floor(Math.random() * (i + 1));
+// 		[deck[i], deck[j]] = [deck[j], deck[i]];
+// 	}
+// 	console.log('here');
+// 	return deck;
+// };
 
 // Generate the full deck
-export const cards = shuffleDeck(generateDeck())
+export const cards = generateDeck()
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,42 @@
 <script>
-    import { cards } from '../lib/cards'
+    import CardsIcon from '../assets/icons/svg/poker-cards.svelte'
+    import ChipsIcon from '../assets/icons/svg/poker-chips.svelte'
+    import { cards } from '../lib/cards';
+
+    const cardDeck = cards;
+    const discardPile = [];
+    const board = [];
+    const hand1 = [];
+    const hand2 = [];
 </script>
 
-<div class="cards-container w-full h-auto overflow-hidden flex flex-wrap gap-3 p-12 bg-green-700">
-    {#each cards as { id, cardSuit, cardValue }, i}
-        <div id="{id}" class="card {cardSuit} relative w-32 h-48 flex justify-center items-center bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold text-7xl">
+<div class="poker-table relative w-screen h-screen overflow-hidden flex flex-wrap gap-3 bg-green-700">
+    <!-- {#each cards as { id, cardSuit, cardValue }, i}
+        <div
+        id="{id}"
+        class="card {cardSuit} relative w-24 h-36 flex justify-center items-center bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold text-6xl"
+        style="--backgroundImage: url(../src/assets/icons/card-suits/{cardSuit}.png)">
             {cardValue}
         </div>
-    {/each}
+    {/each} -->
+    <div class="table-art absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 flex items-center justify-center aspect-video border-8 rounded-full opacity-25 text-white text-8xl font-serif tracking-widest underline underline-offset-[16px]">
+        POKER
+        <div class="icon-wrapper absolute w-8 h-8">
+            <CardsIcon/>
+        </div>
+        <div class="icon-wrapper absolute w-8 h-8">
+            <ChipsIcon/>
+        </div>
+    </div>
 </div>
 
 <style>
-    .card::before,
+    /* .card::before,
     .card::after {
         content: '';
         position: absolute;
-        width: 2.25rem;
-        height: 2.25rem;
+        width: 1.75rem;
+        height: 1.75rem;
         border-radius: 100%;
         background-image: var(--backgroundImage);
         background-size: 100%;
@@ -32,23 +52,10 @@
     .card::after {
         bottom: 0.5rem;
         right: 0.5rem;
-    }
+    } */
 
-    .SPADES {
-        --backgroundImage: url(../assets/icons/card-suits/SPADES.png);
-    }
-
-    .CLUBS {
-        --backgroundImage: url(../assets/icons/card-suits/CLUBS.png);
-    }
-    
-    .HEARTS {
-        color: red;
-        --backgroundImage: url(../assets/icons/card-suits/HEARTS.png);
-    }
-
-    .DIAMONDS {
-        color: red;
-        --backgroundImage: url(../assets/icons/card-suits/DIAMONDS.png);
+    .icon-wrapper svg {
+        width: 100%;
+        height: 100%;
     }
 </style>
