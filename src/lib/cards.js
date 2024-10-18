f5d3011d795fb0f079d08b718a7017b54a7a9544
João Dias <joao.dias@inm.pt>
2024-10-18 01:29:17 +0100

    fixing helper fn

--- src/lib/cards.js
+++ src/lib/cards.js
@@ -1,22 +1,39 @@
-const suits = ['SPADES', 'CLUBS', 'DIAMONDS', 'HEARTS'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const SUITS = ['SPADES', 'CLUBS', 'DIAMONDS', 'HEARTS'];
+const VALUES = [
+	{ face: '2', score: 1 },
+	{ face: '3', score: 2 },
+	{ face: '4', score: 3 },
+	{ face: '5', score: 4 },
+	{ face: '6', score: 5 },
+	{ face: '7', score: 6 },
+	{ face: '8', score: 7 },
+	{ face: '9', score: 8 },
+	{ face: '10', score: 9 },
+	{ face: 'J', score: 10 },
+	{ face: 'Q', score: 11 },
+	{ face: 'K', score: 12 },
+	{ face: 'A', score: 13 }
+]
+
+// import { SUITS, VALUES } from './constants'
 
 const generateDeck = () => {
-	const deck = []
+	const DECK = []
 
 	// Loop through each suit and value to create the cards
-	suits.forEach(suit => {
-		values.forEach(value => {
+	SUITS.forEach(suit => {
+		VALUES.forEach(value => {
 			const card = {
-				id: `${value}_${suit}`,         // Example: 'A_SPADES', '10_HEARTS'
-				cardSuit: suit,                 // Card suit, e.g., 'SPADES', 'HEARTS'
-				cardValue: value                // Card value, e.g., 'A', '10', 'K'
+				id: `${value.face}_${suit}`,
+				cardSuit: suit,
+				cardFace: value.face,
+				cardScore: value.score
 			};
-			deck.push(card)
+			DECK.push(card)
 		})
 	})
 
-	return deck
+	return DECK
 }
 
 // const shuffleDeck = (deck) => {
@@ -29,4 +46,4 @@ const generateDeck = () => {
 // };
 
 // Generate the full deck
-export const cards = generateDeck()
+export const CARDS = generateDeck()
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,25 @@
 <script>
     import CardsIcon from '../assets/icons/svg/poker-cards.svelte'
     import ChipsIcon from '../assets/icons/svg/poker-chips.svelte'
-    import { cards } from '../lib/cards';
+    import { CARDS } from '../lib/cards';
 
-    const cardDeck = cards;
-    const discardPile = [];
-    const board = [];
-    const hand1 = [];
-    const hand2 = [];
+    // const cardDeck = cards;
+    // const discardPile = [];
+    // const board = [];
+    // const hand1 = [];
+    // const hand2 = [];
 </script>
 
 <div class="poker-table relative w-screen h-screen overflow-hidden flex flex-wrap gap-3 bg-green-700">
-    <!-- {#each cards as { id, cardSuit, cardValue }, i}
+    {#each CARDS as { id, cardSuit, cardFace, cardScore }, i}
         <div
         id="{id}"
         class="card {cardSuit} relative w-24 h-36 flex justify-center items-center bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold text-6xl"
-        style="--backgroundImage: url(../src/assets/icons/card-suits/{cardSuit}.png)">
-            {cardValue}
+        style="--backgroundImage: url(../src/assets/icons/card-suits/{cardSuit}.png)"
+        data-score="{cardScore}">
+            {cardFace}
         </div>
-    {/each} -->
+    {/each}
     <div class="table-art absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 flex items-center justify-center gap-4 aspect-video border-8 rounded-full opacity-25 text-white text-8xl font-serif tracking-widest underline underline-offset-[16px]">
         <div class="icon-wrapper h-28">
             <CardsIcon/>
@@ -31,7 +32,7 @@
 </div>
 
 <style>
-    /* .card::before,
+    .card::before,
     .card::after {
         content: '';
         position: absolute;
@@ -52,5 +53,5 @@
     .card::after {
         bottom: 0.5rem;
         right: 0.5rem;
-    } */
+    }
 </style>
