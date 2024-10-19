14fa6be8eeee845d8df8690e199a8f9c4e81f5a0
João Dias <joao.dias@inm.pt>
2024-10-19 18:23:32 +0100

    started working on hand evaluation

--- src/lib/helperFns.js
+++ src/lib/helperFns.js
@@ -6,11 +6,12 @@ export const generateDeck = () => {
 	SUITS.forEach(suit => {
 		VALUES.forEach(value => {
 			const card = {
-				id: `${value.face}_${suit}`,
-				cardSuit: suit,
+				cardID: `${value.face}_${suit.suit}`,
+				cardSuit: suit.suit,
 				cardFace: value.face,
-				cardScore: value.score
-			};
+				cardScore: value.score,
+				bgImg: suit.img
+			}
 			DECK.push(card)
 		})
 	})
@@ -23,11 +24,83 @@ export const getRandomCard = (deck) => {
 		return { selectedCard: null, newDeck: deck }
 	}
 	// Generate a random index
-	const randomIndex = Math.floor(Math.random() * deck.length);
+	const randomIndex = Math.floor(Math.random() * deck.length)
 	// Get the random object from the array
 	const selectedCard = deck[randomIndex]
 	// Remove the selected card from the deck
   	const newDeck = [...deck.slice(0, randomIndex), ...deck.slice(randomIndex + 1)]
 
 	return { selectedCard, newDeck }
+}
+
+export const getWinningHand = (players, board) => {
+	let CURRENT_HIGHSCORE = 0
+	let WINNER_HAND = { player: null, hand: 0 }
+	for (let i = 0; i < players.length; i++) {
+		const CURRENT_PLAYER = players[i]
+		const COMPLETE_PLAYER_HAND = CURRENT_PLAYER.playerHand.concat(board)
+		const SCORED_HAND = evaluateHand(COMPLETE_PLAYER_HAND)
+		if (SCORED_HAND.score > CURRENT_HIGHSCORE) {
+			CURRENT_HIGHSCORE = SCORED_HAND.score
+			WINNER_HAND = { player: CURRENT_PLAYER, score: SCORED_HAND }
+		}
+	}
+
+	return WINNER_HAND
+}
+
+const evaluateHand = (hand) => {
+	const WITH_REPEATS = checkForRepeats(hand)
+	if (WITH_REPEATS) {
+		return WITH_REPEATS
+	}
+	return { score: 0 }
+}
+
+const checkForRepeats = (hand) => {
+	const CARD_FACE_COUNT = {}
+	hand.forEach(card => {
+		const FACE = card.cardFace
+		if (CARD_FACE_COUNT[FACE]) {
+			CARD_FACE_COUNT[FACE]++
+		} else {
+			CARD_FACE_COUNT[FACE] = 1
+		}
+	})
+
+	let PAIRS = []
+
+	// Check for Four of a Kind
+	for (const face in CARD_FACE_COUNT) {
+		if (CARD_FACE_COUNT[face] === 4) {
+			return { score: 400, text: "Four of a Kind", value: face }
+		}
+	}
+
+	// Check for Three of a Kind
+	for (const face in CARD_FACE_COUNT) {
+		if (CARD_FACE_COUNT[face] === 3) {
+			return { score: 300, text: "Three of a Kind", value: face }
+		}
+	}
+
+	// Check for Two Pairs and One Pair
+	for (const face in CARD_FACE_COUNT) {
+		if (CARD_FACE_COUNT[face] === 2) {
+			PAIRS.push(face)
+		}
+	}
+
+	// If there are two pairs, return "Two Pairs"
+	if (PAIRS.length === 2) {
+		return { score: 200, text: "Two Pairs", values: PAIRS }
+	}
+
+	// If there is one pair, return "One Pair"
+	if (PAIRS.length === 1) {
+		return { score: 100, text: "One Pair", value: PAIRS[0] }
+	}
+
+	// If none of the conditions match, return null
+	return null
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
+    import CardGroup from '../components/cardGroup.svelte'
     import CardsIcon from '../assets/icons/svg/poker-cards.svelte'
     import ChipsIcon from '../assets/icons/svg/poker-chips.svelte'
-    import { slide } from 'svelte/transition'
-    import { generateDeck, getRandomCard } from '../lib/helperFns'
+    import { generateDeck, getRandomCard, getWinningHand } from '../lib/helperFns'
     import { NUMBER_OF_PLAYERS, CARDS_PER_PLAYER } from "../lib/constants"
 
     let CARD_DECK = generateDeck()
@@ -10,53 +10,62 @@
     let DISCARD_PILE = []
     let PLAYERS = []
     let DRAW_STAGE = 1
+    let DEAL_TEXT = 'DEAL PLAYERS'
 
     const dealCards = () => {
         if (DRAW_STAGE === 1) {
             dealCardsToPlayers()
             DRAW_STAGE++
+            DEAL_TEXT = 'DEAL FLOP'
             return
         }
-
         if (DRAW_STAGE === 2) {
             burnCard()
             dealCardToBoard()
             dealCardToBoard()
             dealCardToBoard()
             DRAW_STAGE++
+            DEAL_TEXT = 'DEAL TURN'
             return
         }
-
-        if (DRAW_STAGE === 3 || DRAW_STAGE === 4) {
+        if (DRAW_STAGE === 3) {
             burnCard()
             dealCardToBoard()
             DRAW_STAGE++
+            DEAL_TEXT = 'DEAL RIVER'
             return
         }
-
-        if (DRAW_STAGE === 5) return
+        if (DRAW_STAGE === 4) {
+            burnCard()
+            dealCardToBoard()
+            DRAW_STAGE++
+            DEAL_TEXT = 'SHOW RESULTS'
+            return
+        }
+        if (DRAW_STAGE === 5) {
+            const results = getWinningHand(PLAYERS, BOARD_CARDS)
+            console.log(results)
+            DRAW_STAGE++
+            return
+        }
     }
 
     const dealCardToBoard = () => {
-        let TEMP_CARDS = BOARD_CARDS
         if (BOARD_CARDS.length === 5) return
         const { selectedCard, newDeck } = getRandomCard(CARD_DECK)
-        TEMP_CARDS.push(selectedCard)
-
-        BOARD_CARDS = TEMP_CARDS
+        BOARD_CARDS.push(selectedCard)
+        BOARD_CARDS = BOARD_CARDS
         CARD_DECK = newDeck
     }
 
     const dealCardsToPlayers = () => {
         if (NUMBER_OF_PLAYERS > 0 && PLAYERS.length === 0) buildPlayers()
-
         while (PLAYERS.some(player => player.playerHand.length < CARDS_PER_PLAYER)) {
             for (let i = 0; i < PLAYERS.length; i++) {
                 if (PLAYERS[i].playerHand.length < CARDS_PER_PLAYER) {
-                    let TEMP_CARDS = PLAYERS[i].playerHand
                     const { selectedCard, newDeck } = getRandomCard(CARD_DECK)
-                    TEMP_CARDS.push(selectedCard)
-                    PLAYERS[i].playerHand = TEMP_CARDS
+                    PLAYERS[i].playerHand.push(selectedCard)
+                    PLAYERS[i].playerHand = PLAYERS[i].playerHand
                     CARD_DECK = newDeck
                 }
             }
@@ -64,26 +73,24 @@
     }
 
     const burnCard = () => {
-        let TEMP_CARDS = DISCARD_PILE
         const { selectedCard, newDeck } = getRandomCard(CARD_DECK)
-        TEMP_CARDS.push(selectedCard)
-
-        DISCARD_PILE = TEMP_CARDS
+        DISCARD_PILE.push(selectedCard)
+        DISCARD_PILE = DISCARD_PILE
         CARD_DECK = newDeck
     }
 
     const buildPlayers = () => {
-        const TEMP_PLAYERS = []
         for (let index = 1; index <= NUMBER_OF_PLAYERS; index++) {
-            TEMP_PLAYERS.push(
+            PLAYERS.push(
                 { playerID: `PLAYER_${index}`, playerHand: [] }
             )
         }
-        PLAYERS = TEMP_PLAYERS
+        PLAYERS = PLAYERS
     }
 
     const resetAll = () => {
         DRAW_STAGE = 1
+        DEAL_TEXT = 'DEAL PLAYERS'
         CARD_DECK = generateDeck()
         BOARD_CARDS = []
         DISCARD_PILE = []
@@ -97,67 +104,36 @@
 <div class="poker-table relative w-screen h-screen overflow-hidden flex items-center justify-center flex-wrap p-12 bg-green-700">
     <div class="table-top w-full h-full flex flex-col items-center justify-center flex-wrap gap-6 z-10">
         <div class="buttons flex items-center justify-center gap-3">
-            <button
-            on:click={dealCards}
-            class="deal-btn max-h-8 max-w-16 flex items-center justify-center rounded-lg py-6 px-16 bg-white text-xl font-bold shadow-md">DEAL</button>
-            <button
-            on:click={resetAll}
-            class="deal-btn max-h-8 max-w-16 flex items-center justify-center rounded-lg py-6 px-16 bg-white text-xl font-bold shadow-md">RESET</button>
+            {#if DRAW_STAGE !== 6}
+                <button
+                on:click={dealCards}
+                class="deal-btn max-h-8 min-w-16 flex items-center justify-center rounded-lg py-6 px-8 bg-white text-xl font-bold shadow-md">{DEAL_TEXT}</button>
+            {/if}
+
+            {#if DRAW_STAGE !== 1}
+                <button
+                on:click={resetAll}
+                class="deal-btn max-h-8 min-w-12 flex items-center justify-center rounded-lg py-6 px-8 bg-white text-xl font-bold shadow-md">RESET</button>
+            {/if}
         </div>
 
         <!-- BOARD CARDS -->
-        <div class="board-cards h-36 flex flex-col items-center justify-center gap-3">
-            <h1 class="text-xl font-bold">BOARD</h1>
-            <div class="cards-wrapper flex items-center justify-center gap-3">
-                {#each BOARD_CARDS as { id, cardSuit, cardFace, cardScore } (id)}
-                    <div
-                    transition:slide
-                    id="{id}"
-                    class="card {cardSuit} relative w-24 h-36 flex justify-center items-center bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold text-6xl"
-                    style="--backgroundImage: url(../src/assets/icons/card-suits/{cardSuit}.png)"
-                    data-score="{cardScore}">
-                        {cardFace}
-                    </div>
-                {/each}
-            </div>
-        </div>
+        <CardGroup
+        numberOfCards="{5}"
+        label="BOARD"
+        cards="{BOARD_CARDS}"
+        smallCards="{false}" />
 
         <!-- PLAYERS -->
-         <div class="players-wrapper w-full flex items-center justify-between gap-12">
+        <div class="players-wrapper w-full flex items-center justify-between gap-12">
             {#each PLAYERS as { playerID, playerHand } (playerID)}
-                <!-- PLAYER CARDS -->
-                <div class="board-cards flex flex-col items-center justify-center gap-3">
-                    <h1 class="text-xl font-bold">{playerID}</h1>
-                    <div class="cards-wrapper h-36 flex items-center justify-center gap-3">
-                        {#each playerHand as { id, cardSuit, cardFace, cardScore } (id)}
-                            <div
-                            transition:slide
-                            id="{id}"
-                            class="card card-small {cardSuit} relative w-16 h-2/3 flex justify-center items-center bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold text-5xl"
-                            style="--backgroundImage: url(../src/assets/icons/card-suits/{cardSuit}.png)"
-                            data-score="{cardScore}">
-                                {cardFace}
-                            </div>
-                        {/each}
-                    </div>
-                </div>
+                <CardGroup
+                numberOfCards="{2}"
+                label="{playerID}"
+                cards="{playerHand}"
+                smallCards="{true}" />
             {/each}
-         </div>
-
-        <!-- DISCARD CARDS -->
-        <!-- <div class="discard-cards h-36 flex items-center justify-center gap-3">
-            DISCARD
-            {#each DISCARD_PILE as { id, cardSuit, cardFace, cardScore } (id)}
-                <div
-                transition:slide
-                id="{id}"
-                class="card card-small {cardSuit} relative w-16 h-2/3 flex justify-center items-center bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold text-5xl"
-                style="--backgroundImage: url(../src/assets/icons/card-suits/{cardSuit}.png)"
-                data-score="{cardScore}">
-                    {cardFace}
-                </div>
-            {/each}
-        </div> -->
+        </div>
     </div>
 
     <div class="table-art absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 flex items-center justify-center gap-4 aspect-video border-8 rounded-full opacity-25 text-white text-8xl font-serif tracking-widest underline underline-offset-[16px]">
@@ -170,44 +146,3 @@
         </div>
     </div>
 </div>
-
-<style>
-    .card::before,
-    .card::after {
-        content: '';
-        position: absolute;
-        width: 1.75rem;
-        height: 1.75rem;
-        border-radius: 100%;
-        background-image: var(--backgroundImage);
-        background-size: 100%;
-        background-position: center;
-        background-repeat: no-repeat;
-    }
-
-    .card::before {
-        top: 0.5rem;
-        left: 0.5rem;
-    }
-
-    .card::after {
-        bottom: 0.5rem;
-        right: 0.5rem;
-    }
-
-    .card-small::before,
-    .card-small::after {
-        width: 1.15rem;
-        height: 1.15rem;
-    }
-
-    .card-small::before {
-        top: 0.25rem;
-        left: 0.25rem;
-    }
-
-    .card-small::after {
-        bottom: 0.25rem;
-        right: 0.25rem;
-    }
-</style>
