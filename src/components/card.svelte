6de77b906f3cf741749212e8f7e2d89ec1bb1b3e
João Dias <joao.dias@inm.pt>
2024-10-28 09:17:01 +0000

    added padding bottom to card

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -18,7 +18,7 @@
 <div
 in:fly={flyParams}
 id="{cardID}"
-class="card {cardSuit} {isSmall ? 'card-small w-16 h-24 text-5xl' : 'w-24 h-36 text-6xl'} relative flex justify-center items-center bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold"
+class="card {cardSuit} {isSmall ? 'card-small w-16 h-24 text-5xl' : 'w-24 h-36 text-6xl'} relative flex justify-center items-center pb-[5px] bg-slate-50 rounded-md border-black border-2 shadow-xl font-sans font-bold"
 style="--backgroundImage: url({bgImg})"
 data-score="{cardScore}">
     {cardFace}
